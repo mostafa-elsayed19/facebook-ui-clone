11debb2881e96425641203a5fe107e0dfcadfe38
Mostafa El-Sayed <mostafaelsayed19@outlook.com>
2024-07-09 05:18:47 +0300

    added props description for button and formcard

--- src/components/reusable/Button.jsx
+++ src/components/reusable/Button.jsx
@@ -1,3 +1,9 @@
+/**
+ *
+ * @param {string} className - for more tailwind customizations
+ * @param {string} children - Button text
+ * @returns
+ */
 function Button({ className, children }) {
 	return (
 		<button className={`ml-2 px-4 py-1 rounded ${className}`}>
--- src/components/reusable/FormCard.jsx
+++ src/components/reusable/FormCard.jsx
@@ -1,3 +1,10 @@
+/**
+ *
+ * @param {string} className - for more tailwind customizations
+ * @param {data} children - Form card elements
+ * @returns
+ */
+
 function FormCard({ className, children }) {
 	return (
 		<div className={`bg-white rounded-lg shadow-2xl max-w-md ${className}`}>
