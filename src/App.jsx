125394c7b50bf8bbfa04b2f4c31d3e13c0611019
Nada Ahmed <nada.ahmed2108@gmail.com>
2024-07-12 18:40:53 +0300

    browser router

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,33 @@
-// import { useState } from "react";
-import "./index.css"
+import React from "react";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom"; 
+import "./index.css";
 import Footer from "./components/Footer";
 import Login from "./pages/login";
+import SignUp from "./pages/signUp";
 // import Input from "./components/reusable/Input";
 
+const AppLayOut = () => (
+  <>
+    <Outlet />
+    <Footer />
+  </>
+);
+
+const router = createBrowserRouter([
+  {
+    path: '/', element: <AppLayOut />,
+    children: [
+		{ index: true, element: <Login /> }, // مؤقتا لغاية مانشوف ايه ال هيبقا هوووم 
+		{ path: 'login', element: <Login /> },
+		{path:'SignUp', element: <SignUp />}
+	]
+  }
+]);
+
 function App() {
-	return (
-		<>
-			{/* <Input type="text" placeholder="Email address or phone" />
-			<Input type="password" placeholder="Password" /> */}
-			<Login/>
-			<Footer />
-		</>
-	);
+  return (
+    <RouterProvider router={router} />
+  );
 }
 
 export default App;
